5b09cfd9caa5ba57a45850ae2fd2756317b80d8f
kerimoglutolga <tolgakerimoglu@outlook.com>
2020-03-21 15:20:01 +0300

    address decoder

--- main.cpp
+++ main.cpp
@@ -3,26 +3,45 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
-#include <unordered_set>
+
 using namespace std;
 
-int ax=0, bx=0, cx=0, dx=0, al=0, ah=0, bl=0, bh=0, cl=0, ch=0, dl=0, dh=0, di=0, sp=0xfffe, si=0, bp=0; //registers
-
-int zf=0, cf=0, af=0, sf=0, df=0; //flags
-
 vector<string> tokens;
-
 vector<int> memory(2<<15, -1);
 // initializes memory and labels
 unordered_map<string, int> labels; // maps from label to memory address
 unordered_map<string, pair<int, string>> variables; // maps from variable name to a pair of it's memory address and string value
-unordered_map<string, int&> generalRegisters;
+string instructions = "NOP,NOT,JZ,JNZ,JE,JNE,JA,JAE,JB,JBE,JNAE,JNB,JNBE,JNC,JC,PUSH,POP,INT,MOV,ADD,SUB,MUL,DIV,XOR,OR,AND,RCL,RCR,SHL,SHR";
+string directives ="DW,DB";
+// need to add lowercase versions
+unordered_map<string, pair<int, int>> registers; // maps the name of the register to a pair of it's value and size in bits
+
+
+// flags
+int ZF = 0,AF = 0,CF = 0,SF = 0,OF= 0;
+
+string addressDecoder(string token);
 
 int main() {
-
-    unordered_map<string, int&> registers= { {"ax",ax} , {"bx", bx}, {"cx", cx}, {"dx",dx}, {"al",al},{"ah",ah}, {"bl",bl},{"bh",bh},{"cl",cl},{"ch",ch},{"dl",dl},{"dh",dl}}; //contains ax, bx... al, ah...
-    unordered_map<string, int&> generalRegisters = registers; // bad style
-
+    // initiate registers and flags
+    // 8 bit registers
+    registers["AH"] = make_pair(0,8);
+    registers["AL"] = make_pair(0,8);
+    registers["BH"] = make_pair(0,8);
+    registers["BL"] = make_pair(0,8);
+    registers["CH"] = make_pair(0,8);
+    registers["CL"] = make_pair(0,8);
+    registers["DH"] = make_pair(0,8);
+    registers["DL"] = make_pair(0,8);
+    // 16 bit registers
+    registers["AX"] = make_pair(0,16);
+    registers["BX"] = make_pair(0,16);
+    registers["CX"] = make_pair(0,16);
+    registers["DX"] = make_pair(0,16);
+    registers["DI"] = make_pair(0,16);
+    registers["SP"] = make_pair((int)0xFFFE,16);
+    registers["SI"] = make_pair(0,16);
+    registers["BP"] = make_pair(0,16);
     // parses the input program and places the tokens into a vector
     ifstream inFile;
     inFile.open("../test.txt");
@@ -41,10 +60,6 @@ int main() {
                 tokens.push_back(line.substr(prev, string::npos));
         }
     }
-
-    string instructions = "NOP,NOT,JZ,JNZ,JE,JNE,JA,JAE,JB,JBE,JNAE,JNB,JNBE,JNC,JC,PUSH,POP,INT,MOV,ADD,SUB,MUL,DIV,XOR,OR,AND,RCL,RCR,SHL,SHR";
-    string directives ="DW,DB";
-    // need to add lowercase versions
     int curPos = 0;
     for (int i=0; i<tokens.size(); i++) {
         string curToken = tokens[i];
@@ -55,13 +70,13 @@ int main() {
                 curPos++;
             }
         }
-        // if the token is a label encode the memory location it refers to
+            // if the token is a label encode the memory location it refers to
         else if (curToken.back() == ':') {
             curToken.pop_back();
             labels[curToken] = curPos;
             continue;
         }
-        // if the token is a directive encode the variable in memory and put it's address and string value to the variable map
+            // if the token is a directive encode the variable in memory and put it's address and string value to the variable map
         else if (directives.find(curToken) != string::npos) {
             variables[tokens[i - 1]] = make_pair(curPos, tokens[i + 1]);
             if (tokens[i + 1] == "OFFSET") {
@@ -72,21 +87,22 @@ int main() {
             curPos++;
             if (curToken == "DW") {
                 memory[curPos] = i;
-                    curPos++;
+                curPos++;
             }
         }
     }
-    for (int i=0; i<memory.size(); i++) {
-        cout << memory[i] << " ";
-        if(memory[i] == -1) break;
+
+}
+
+string addressDecoder(string token) {
+    // register addressing: if operand is the contents of a register return the name of the registers in a string
+    if (registers.find(token) != registers.end()) {
+        return token;
     }
-
-
-
-
-
-
-
-
-
+    // register indirect: if the operand is the contents of the memory address pointed to by a register, return the token corresponding to that memory address
+    if (token.front() == '[' && token.back() == ']' && registers.find(token.substr(1,2)) != registers.end()) {
+        return tokens[memory[registers[token]]];
+    }
+    // memory addressing: if the operand is the content of a memory location return the token corresponding to that memory location
+    
 }
